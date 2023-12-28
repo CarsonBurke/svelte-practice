e8804337839889c8142da0d9fbeb8a69eaf343e1
CarsonBurke <carsonburke22@gmail.com>
2023-12-27 17:01:59 -0800

    initial project structure

---
+++ src/routes/+page.svelte
@@ -0,0 +1,13 @@
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<button on:click={increment}>You have {i} apples and {i2} oranges</button>
+
+<script>
+    let i = 0
+    function increment() {
+
+        i += 1
+    }
+    $: i2 = i * 2
+</script>
